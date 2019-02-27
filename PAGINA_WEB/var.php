3da7415e14d269362d3f89dd5e21a819eaf398c8
Alejandro314 <alejandro_roman_caballero@outlook.es>
2019-02-27 17:07:09 +0100

    Cambios en var.php

--- PAGINA_WEB/var.php
+++ PAGINA_WEB/var.php
@@ -1,17 +1,11 @@
 <?php
 
 
-  //Checking if exist the var
-  //if (isset($_ENV['MYSQL_USER'])) {
-    //$db_user=$_ENV['MYSQL_USER']; 
-    //$db_host=$_ENV['MYSQL_HOST']; 
-    //$db_password=$_ENV['MYSQL_PASSWORD']; 
-    //$db_name=$_ENV["MYSQL_DB"]; 
-  //} else {
-    $db_user="usuario"; //my db user
+
+    $db_user="condoriano"; //my db user
     $db_host="localhost"; //my db host
-    $db_password="2asirtriana"; //my db password
+    $db_password="890iopklñ"; //my db password
     $db_name="alebuntu"; //my db name
-  //}
+
 
 ?>
