d36d7b2193db8a1dba632589d14375c3e228045f
Alejandro314 <alejandro_roman_caballero@outlook.es>
2019-03-04 16:18:22 +0100

    La ñ pasa a ser una n en la contraseña de condoriano porque puede dar bug

--- PAGINA_WEB/var.php
+++ PAGINA_WEB/var.php
@@ -4,7 +4,7 @@
 
     $db_user="condoriano"; //my db user
     $db_host="localhost"; //my db host
-    $db_password="890iopklñ"; //my db password
+    $db_password="890iopkln"; //my db password
     $db_name="alebuntu"; //my db name
 
 
